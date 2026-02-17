db4a7ab347c3efa7aea7e7743d5cdc59adcb7d37
Hlib-Rachkovskyy <163143242+Hlib-Rachkovskyy@users.noreply.github.com>
2026-02-17 21:34:23 +0100

    FIX untracked build mistakes

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import datetime
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 class Config:
-    SECRET_KEY = 'you-will-never-guess'
-    # Format: postgresql://username:password@host:port/database
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:mysecretpassword@localhost:5431/postgres'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key-only')
     JWT_SECRET_KEY = 'your_jwt_secret_key'
     JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(minutes=5)
     JWT_TOKEN_LOCATION = ['cookies']
